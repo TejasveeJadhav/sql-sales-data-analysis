7c8d283c8a919e01dc13df0e947fc02c332f10d3
Tejasvee Jadhav <tejsvee1308@gmail.com>
2026-07-13 00:30:29 +0530

    Create SalesAnalysisDB and Sales table

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,1 +1,17 @@
+CREATE DATABASE SalesAnalysisDB;
+GO
 
+USE SalesAnalysisDB;
+GO
+
+CREATE TABLE Sales (
+    OrderID INT PRIMARY KEY,
+    OrderDate DATE,
+    CustomerName VARCHAR(100),
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    Region VARCHAR(50),
+    Quantity INT,
+    SalesAmount DECIMAL(10,2),
+    Profit DECIMAL(10,2)
+);
